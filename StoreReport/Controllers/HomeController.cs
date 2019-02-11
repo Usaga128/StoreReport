99cbafd0cdd8de0e4835f6b9e788ea786687e748
Esteban Usaga <estebanusaga@hotmail.com>
2019-02-10 19:40:11 -0600

    Inicio el reporteador de información
    Se inicia modificando la vista que corresponde al reporteador.

--- StoreReport/Controllers/HomeController.cs
+++ StoreReport/Controllers/HomeController.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StoreReport.Data;
 using StoreReport.Models;
 
 namespace StoreReport.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            LoadViewBag();
             return View();
         }
 
         public IActionResult Register()
         {
+            LoadViewBag();
+
             ViewData["Message"] = "";
 
             return View();
@@ -31,7 +42,25 @@ namespace StoreReport.Controllers
 
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return null;
+        }
+
+        public void LoadViewBag()
+        {
+
+            var productlist = _context.Store.OrderBy(modelType => modelType.Name).ToList();
+            ViewBag.storelist = new SelectList(productlist, "StoreID", "Name");
+
+            var routelist = _context.Route.OrderBy(modelType => modelType.RouteName).ToList();
+            ViewBag.routelist = new SelectList(routelist, "RouteID", "RouteName");
+
+            var itemlist = _context.Item.OrderBy(modelType => modelType.ItemID).ToList();
+            ViewBag.itemlist = new SelectList(itemlist, "ItemID", "Name");
+
+            var checklist = _context.Checks.OrderBy(modelType => modelType.ChecksID).ToList();
+            ViewBag.checklist = new SelectList(checklist, "ChecksID", "Question");
+
+
         }
     }
 }
